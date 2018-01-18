d07740cef5ebc9815b4f252240f15920025632ea
jkubrynski <jkubrynski@gmail.com>
2018-01-18 12:58:06 +0100

    Remove rest

--- src/test/java/com/acme/ecommerce/warehouse/contracts/BaseContractTest.java
+++ src/test/java/com/acme/ecommerce/warehouse/contracts/BaseContractTest.java
@@ -1,12 +1,7 @@
 package com.acme.ecommerce.warehouse.contracts;
 
-import com.acme.ecommerce.warehouse.stocks.ProductStock;
-import com.acme.ecommerce.warehouse.stocks.StockController;
-import com.acme.ecommerce.warehouse.stocks.StockService;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
-import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,13 +11,5 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standal
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public abstract class BaseContractTest {
-
-	@Before
-	public void setUp() {
-		StockService stockService = Mockito.mock(StockService.class);
-		Mockito.when(stockService.checkStock(Mockito.anyInt())).thenReturn(new ProductStock(34257815, true));
-
-		RestAssuredMockMvc.standaloneSetup(standaloneSetup(new StockController(stockService)));
-	}
 
 }
